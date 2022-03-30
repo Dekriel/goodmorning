8e7e6ad52866abbfc49b05b450ec0b85f8753af6
Dekriel <92822789+Dekriel@users.noreply.github.com>
2022-03-30 08:45:23 +0100

    fix

--- gm.py
+++ gm.py
@@ -7,10 +7,8 @@ response = requests.get(query)
 temp = []
 scraped = BeautifulSoup(response.text, 'html.parser')
 headlines = scraped.find('body').find_all('h3')
-link = scraped.find('body').find_all('a')
 for i in headlines:
-    for j in link:
-        temp.append(i.text.strip() + '         ')
+    temp.append(i.text.strip())
 
 print("good morning!")
 print("=================================TOP STORIES TODAY=================================")
